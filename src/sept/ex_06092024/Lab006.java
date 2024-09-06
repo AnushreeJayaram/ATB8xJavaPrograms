bf529e5593fa9c5c6db81cecc97532c01299a907
AnushreeJayaram <anushreej3295@gmail.com>
2024-09-06 08:49:22 +0530

    Save the changes, add the files

---
+++ src/sept/ex_06092024/Lab006.java
@@ -0,0 +1,12 @@
+package sept.ex_06092024;
+
+public class Lab006 {
+    public static void main(String[] anu) {
+        // args can also be written as anything
+        System.out.println("Hello World");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,23 @@
+package sept.ex_06092024;
+
+public class Lab007 {
+    /**
+     * hello
+     * @Author: Anu
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        //    this code will not be executed
+        //    understanding , show for the user
+        //    you can write anything
+
+        System.out.println("hi all");
+
+        /*
+
+        Multi line comment
+
+         */
+    }
+}
